9ef7147fc0ac79aa6ce52becf656cc6617b4b4ef
mingi jang <jangwonh4@naver.com>
2017-07-13 18:17:34 -0400

    초기 커밋.

---
+++ Abstract/GiantPlanet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class GiantPlanet : Planet
+    {
+        private string _type;
+        public GiantPlanet(string name,double diameter, double mass, string type)
+        {
+            
+        }
+            
+
+    }
+}
---
+++ Abstract/Planet.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class Planet
+    {
+        private readonly double _diameter;
+        private readonly double _mass;
+        private int _moonCount;
+        private readonly string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+
+
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Name:{0}\nDiameter:{1}\nMass:{2}",Name,Diameter,Mass);
+        }
+
+
+    }
+}
